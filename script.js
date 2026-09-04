d173f2965e5aa69b00f401df3813b048ba826a19
Astor Bascuñan <astorbascunan@gmail.com>
2026-09-04 15:22:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+function pedirApitemp(respuesta){
+    fectch(10.10.10.3:3000/api/temperatura)
+        then(repuesta =>respuesta.Jason())
+        then(data=>{
+            return data.temperatura;
+        }
+}
+function pedirhumedad(respuesta){
+      fectch(10.10.10.3:3000/api/temperatura)
+        then(repuesta =>respuesta.Jason())
+        then(data=>{
+            return data.humedad;
+        }
+      
+}
+function pedirEstado(){
+      fectch(10.10.10.3:3000/api/temperatura)
+        .then(repuesta =>respuesta.Jason())
+        .then(data=>{
+            return data.estado;
+        }
+      
+}
+let temp=0;
+let hume=0;
+let estado;
+let apiT= pedirApi();
+document.write("Temperatura: ", apiT);
+let apiH= pedirhumedad();
+document.write("Humedad: ", apiH);
+let apiE=pedirEstado();
+document.write("Estado: ", apiE);
 
+    
